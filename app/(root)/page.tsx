846a2d043ef2d7ef29b05b71a47c0bc5e27b1ca7
Emin Kumru <eminkumru135@outlook.com>
2023-09-18 19:58:21 +0300

    environment setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline" size="default">
+        Default
+      </Button>
+      <div>Hello Admin Dashboard</div>
+    </div>
+  );
+}
